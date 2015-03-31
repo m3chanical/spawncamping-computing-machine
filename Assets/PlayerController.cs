adff37e224ed72fb54259c37fe820651b3705352
Carl <m3chanical@tinkerduck.com>
2015-03-30 23:11:33 -0400

    add player controller script, move player right

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+		transform.position += transform.right * Time.deltaTime; 	
+	}
+}
