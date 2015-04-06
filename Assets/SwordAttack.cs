546d69a5d3f308396e2b6bc0c7b3013147736327
Carl <m3chanical@tinkerduck.com>
2015-04-05 20:40:21 -0400

    add enemy (no script), add player weapon collider animation

---
+++ Assets/SwordAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordAttack : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other){
+		if(other.tag == "Enemy"){
+			print("Ouch! You hit me.");
+			// Summon up Enemy Damage, subtract enemy health
+			// Summon up Enemy Knockback script. 
+			// Possibly craft up enemy health bar? 
+		}
+	}
+}
